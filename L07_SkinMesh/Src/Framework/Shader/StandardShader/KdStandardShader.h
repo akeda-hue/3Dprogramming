dd4e6aa07faef3aeccd5e415219006d0e6352a05
akeda-hue <kd1359153@st.kobedenshi.st.jp>
2024-08-29 11:04:25 +0900

    スキンメッシュアニメーション対応

--- L07_SkinMesh/Src/Framework/Shader/StandardShader/KdStandardShader.h
+++ L07_SkinMesh/Src/Framework/Shader/StandardShader/KdStandardShader.h
@@ -8,6 +8,9 @@ class KdStandardShader
 {
 public:
 
+	//スキンメッシュ対応
+	static const int maxBoneBufferSize = 300;
+
 	// 定数バッファ(オブジェクト単位更新)
 	struct cbObject
 	{
@@ -21,12 +24,14 @@ public:
 		// エミッシブのみの描画
 		int				OnlyEmissie = 0;
 
+		//スキンメッシュオブジェクトかどうか(スキンメッシュ対応)
+		int				IsSkinMeshObj = 0;
+
 		// ディゾルブ関連
 		float			DissolveThreshold = 0.0f;	// 0 ～ 1
 		float			DissolveEdgeRange = 0.03f;	// 0 ～ 1
 
 		Math::Vector3	DissolveEmissive = { 0.0f, 1.0f, 1.0f };
-		float			_blank2 = 0.0f;
 	};
 
 	// 定数バッファ(メッシュ単位更新)
@@ -45,6 +50,14 @@ public:
 
 		float			Roughness = 1.0f;
 		float			_blank[3] = { 0.0f, 0.0f ,0.0f };
+
+
+	};
+
+	// 定数バッファ(ボーン単位更新:スキンメッシュ対応)
+	struct cbBone
+	{
+		Math::Matrix mBones[300];
 	};
 
 	//================================================
@@ -126,6 +139,9 @@ public:
 
 	const cbMesh& MeshCB() const { return m_cb1_Mesh.Get(); }
 
+	//スキンメッシュ対応
+	const cbBone& WorkBoneCB()const { return m_cb3_Bone.Get(); }
+
 	const cbMaterial& WorkMaterialCB() const { return m_cb2_Material.Get(); }
 
 	//================================================
@@ -193,6 +209,16 @@ private:
 	// 定数バッファを初期状態に戻す
 	void ResetCBObject();
 
+	//スキンメッシュが有効かどうか(スキンメッシュ対応)
+	void SetIsSkinMeshObj(const bool isSkinMeshObj)
+	{
+		if (m_cb0_Obj.Work().IsSkinMeshObj != isSkinMeshObj)
+		{
+			m_cb0_Obj.Work().IsSkinMeshObj = isSkinMeshObj;
+			m_dirtyCBObj = true;
+		}
+	}
+
 	// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
 	// Lit：陰影をつけるオブジェクトの描画用（不透明な物体やキャラクタの板ポリなど
 	// 平行光・点光源などの影響を受け角度によって色を変化させるオブジェクトを描画するシェーダー
@@ -224,6 +250,7 @@ private:
 	KdConstantBuffer<cbObject>		m_cb0_Obj;				// オブジェクト単位で更新
 	KdConstantBuffer<cbMesh>		m_cb1_Mesh;				// メッシュ毎に更新
 	KdConstantBuffer<cbMaterial>	m_cb2_Material;			// マテリアル毎に更新
+	KdConstantBuffer<cbBone>		m_cb3_Bone;				//ボーン毎に(更新スキンメッシュ対応)
 
 	KdRenderTargetPack	m_depthMapFromLightRTPack;
 	KdRenderTargetChanger m_depthMapFromLightRTChanger;
